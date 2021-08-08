417afbf4c9cdae2b482231100e673ee70f361af8
Bunny0007 <shivamsingh789007@gmail.com>
2021-08-08 16:46:07 +0530

    app.js removed console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log("Hi this is clict side JS");
-
-// fetch('http://localhost:3000/weather?address=Jaipur').then((response)=>{
-//     response.json().then((data)=>{
-//         if(data.error){
-//             console.log(error);
-//         }else{
-//             console.log(data.location);
-//             console.log(data.forecast)
-//         }
-//     })
-// })
-
 const formData = document.querySelector('form');
 const search = document.querySelector('input');
 const paraOne = document.querySelector('#one');
