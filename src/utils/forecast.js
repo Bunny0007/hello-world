6fa986380c26401b70065768581e2b80fd224ff3
Bunny0007 <shivamsingh789007@gmail.com>
2021-08-08 22:30:53 +0530

    humidity error fixed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " +body.current.temperature + " degress out. It feels Like "+body.current.feelslike+" ,With humidity "+body.temperature.humidity+"%." )
+            callback(undefined,body.current.weather_descriptions[0] + ". It is currently " +body.current.temperature + " degress out. It feels Like "+body.current.feelslike+" ,With humidity "+body.current.humidity+"%." )
         }
     })
 }
